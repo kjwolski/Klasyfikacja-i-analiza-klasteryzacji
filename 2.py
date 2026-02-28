913d6abc3921d2ee49e5f0997451a550db8ba7da
kjwolski <karol.janusz.wolski@gmail.com>
2026-02-28 11:32:43 +0100

    Check accuracy score

--- 2.py
+++ 2.py
@@ -6,11 +6,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from sklearn import tree
+import numpy as np
 
 data = load_iris()
 X, y = data.data, data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
@@ -37,4 +38,10 @@ plt.grid(True)
 plt.xlabel('Wartość k (odpytywana liczba sąsiadów)')
 plt.ylabel('Dokładność')
 plt.title('Dokładność kNN dla różnych wartości k (liczby sąsiadów)')
-plt.show()
+# plt.show()
+
+optimal_k = k_range[np.argmax(scores)]
+knn_classifier = KNeighborsClassifier(n_neighbors=optimal_k)
+knn_classifier.fit(X_test_scaled, y_test)
+y_pred = knn_classifier.predict(X_test_scaled)
+print(f"Dokładność KNN: {accuracy_score(y_test, y_pred)}")
