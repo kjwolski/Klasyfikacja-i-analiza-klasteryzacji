e5b48b3f82cf5fe1ac16fc76271cfcf4b0196935
kjwolski <karol.janusz.wolski@gmail.com>
2026-02-28 12:09:10 +0100

    Remove useless imports

--- 2.py
+++ 2.py
@@ -3,9 +3,7 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
-from sklearn import tree
 import numpy as np
 
 data = load_iris()
