bfc37654a22a740b819e795a096d4642f323a30a
kjwolski <karol.janusz.wolski@gmail.com>
2026-03-01 09:43:59 +0100

    Add inertia and silhouette scores

--- 4.py
+++ 4.py
@@ -1,4 +1,6 @@
+from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 
 X_blobs, y_true = make_blobs(n_samples=600, centers=4,
@@ -9,4 +11,35 @@ plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=y_true)
 plt.title('Prawdziwe etykiety')
 plt.xlabel('Cecha 1')
 plt.ylabel('Cecha 2')
+plt.show()
+
+inertias = []
+silhouette_scores = []
+K_range = range(2,11)
+
+for k in K_range:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_blobs)
+    inertias.append(kmeans.inertia_)
+    silhouette_scores.append(silhouette_score(X_blobs, kmeans.labels_))
+
+print(inertias)
+
+
+
+plt.figure(figsize=(8,5))
+plt.plot(K_range, silhouette_scores)
+plt.title("Współczynnik sylwetkowy dla liczby klastrów (k)")
+plt.xlabel("Ilość klastrów")
+plt.ylabel("Wartość współczynnika")
+plt.grid(True)
+plt.show()
+
+
+plt.figure(figsize=(8,5))
+plt.plot(K_range, inertias)
+plt.title("Współczynnik sylwetkowy dla liczby klastrów (k)")
+plt.xlabel("Ilość klastrów")
+plt.ylabel("Wartość współczynnika")
+plt.grid(True)
 plt.show()
